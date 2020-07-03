d95e8fc24c7583fce7ca800f41efc0ebe3681128
Philipp Kratzer <philipp.kratzer@ipvs.uni-stuttgart.de>
2020-07-03 20:27:20 +0200

    script to download mogaze files

---
+++ mogaze.sh
@@ -0,0 +1,117 @@
+#!/bin/sh
+mkdir -p mogaze
+mkdir -p mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_2_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_2_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_2_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_2_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p1_2_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p2_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p2_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p2_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p2_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p2_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p3_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p3_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p3_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p3_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p4_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p4_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p4_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p4_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p4_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p5_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p5_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p5_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p5_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p5_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_2_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_2_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_2_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_2_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p6_2_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_objects.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_objects.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_1_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_2_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_2_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_2_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_2_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_2_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_3_gaze_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_3_human_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_3_instructions.csv -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_3_object_data.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/p7_3_segmentations.hdf5 -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/scene.xml -P mogaze
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/bowl.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/chair.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/cup.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/jug2.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/jug.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/knife.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/plate.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/plate_white.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/shelf_laiva.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/shelf_vesken.obj -P mogaze/meshfiles
+wget https://ipvs.informatik.uni-stuttgart.de/mlr/philipp/mogaze/meshfiles/table.obj -P mogaze/meshfiles
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
